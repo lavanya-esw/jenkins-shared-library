57846d01f1146c1fd1c2d339718b288bc73b2574
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-12-31 18:24:36 +0530

    jenkins shared pipeline

--- vars/EKSdeploy.groovy
+++ vars/EKSdeploy.groovy
@@ -1,30 +1,70 @@
+// def call(Map configMap) {
+//     pipeline{
+//         agent{
+//             node{
+//                 label 'AGENT-1'
+//             }
+//         }
+//         environment{
+//             appVersion = configMap.get("appVersion")
+//             ACC_ID = "500532068743"
+//             PROJECT = configMap.get("project")
+//             COMPONENT = configMap.get("component")
+//             deploy_to = configMap.get("deploy_to")
+//             REGION = "us-east-1"
+//         }
+//         stages{
+//             stage('Deploy'){
+//                 steps{
+//                     script{
+//                         withAWS(region:'us-east-1',credentials:'aws_access') {
+//                             sh """
+//                                 set -e
+//                                 aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
+//                                 kubectl get nodes
+//                                 sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
+//                                 helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
+//                         }    """
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
 def call(Map configMap) {
-    pipeline{
-        agent{
-            node{
+    pipeline {
+        agent {
+            node {
                 label 'AGENT-1'
             }
         }
-        environment{
+
+        environment {
             appVersion = configMap.get("appVersion")
-            ACC_ID = "500532068743"
-            PROJECT = configMap.get("project")
-            COMPONENT = configMap.get("component")
-            deploy_to = configMap.get("deploy_to")
-            REGION = "us-east-1"
+            ACC_ID     = "500532068743"
+            PROJECT    = configMap.get("project")
+            COMPONENT  = configMap.get("component")
+            deploy_to  = configMap.get("deploy_to")
+            REGION     = "us-east-1"
         }
-        stages{
-            stage('Deploy'){
-                steps{
-                    script{
-                        withAWS(region:'us-east-1',credentials:'aws_access') {
+
+        stages {
+            stage('Deploy') {
+                steps {
+                    script {
+                        withAWS(region: REGION, credentials: 'aws_access') {
                             sh """
                                 set -e
                                 aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
                                 kubectl get nodes
                                 sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
-                                helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
-                        }    """
+                                helm upgrade --install ${COMPONENT} \
+                                  -f values-${deploy_to}.yaml \
+                                  -n ${PROJECT} \
+                                  --atomic --wait --timeout=5m .
+                            """
+                        }
                     }
                 }
             }
